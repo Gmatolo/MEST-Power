9ec702ab7401daa73472369f65a3ed2fd7fa8787
gmatolo <mulimatolo@gmail.com>
2022-11-01 13:11:51 +0000

    Adds Product Manager Class

--- assignment_3/product_abstract.py
+++ assignment_3/product_abstract.py
@@ -1,24 +1,29 @@
 from abc import ABC, abstractmethod
 
+
 class Product:
-    id=''
-    name=''
-    description=''
-    price=''
-    image=''
-    expiry_date=''
-    date_created=''
+    id = ""
+    name = ""
+    description = ""
+    price = ""
+    image = ""
+    expiry_date = ""
+    date_created = ""
+
 
 class ProductAbstract(ABC):
     @abstractmethod
     def create_product(self, product: Product):
         pass
+
     @abstractmethod
     def edit_product(self):
         pass
+
     @abstractmethod
     def get_product_by_id(self, id):
         pass
+
     @abstractmethod
     def get_all_products(self):
         pass
@@ -26,3 +31,23 @@ class ProductAbstract(ABC):
     @abstractmethod
     def delete_product(self, id):
         pass
+
+
+class ProductManager(ProductAbstract):
+    def create_product(self, product: Product):
+        print("product created")
+
+    def edit_product(self, name):
+        print("product edited")
+
+    def get_product_id(self, id):
+        print("product id")
+
+    def get_all_products(self):
+        print("names of all print")
+
+    def upload_product_image(self, image):
+        print("image uploaded")
+
+    def delete_product(self, id):
+        print("product deleted")
