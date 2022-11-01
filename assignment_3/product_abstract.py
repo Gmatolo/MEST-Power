e10cb82f99f76a16c15d77b4242f50a44aa855f5
gmatolo <mulimatolo@gmail.com>
2022-11-01 12:43:00 +0000

    Adds product abstract class

---
+++ assignment_3/product_abstract.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+
+class Product:
+    id=''
+    name=''
+    description=''
+    price=''
+    image=''
+    expiry_date=''
+    date_created=''
+
+class ProductAbstract(ABC):
+    @abstractmethod
+    def create_product(self):
+        pass
+    @abstractmethod
+    def edit_product(self):
+        pass
+    @abstractmethod
+    def get_product_by_id(self):
+        pass
+    @abstractmethod
+    def get_all_products(self):
+        pass
+
+    @abstractmethod
+    def delete_product(self):
+        pass
