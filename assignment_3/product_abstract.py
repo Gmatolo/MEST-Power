89bfeb97e33a746b53e98cfa78f2ab9e32749489
gmatolo <mulimatolo@gmail.com>
2022-11-01 13:10:35 +0000

    updates abstract product class

--- assignment_3/product_abstract.py
+++ assignment_3/product_abstract.py
@@ -11,18 +11,18 @@ class Product:
 
 class ProductAbstract(ABC):
     @abstractmethod
-    def create_product(self):
+    def create_product(self, product: Product):
         pass
     @abstractmethod
     def edit_product(self):
         pass
     @abstractmethod
-    def get_product_by_id(self):
+    def get_product_by_id(self, id):
         pass
     @abstractmethod
     def get_all_products(self):
         pass
 
     @abstractmethod
-    def delete_product(self):
+    def delete_product(self, id):
         pass
