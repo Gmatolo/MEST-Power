6c8ca865e29209dcf487e6e628f6c62263dce94d
gmatolo <mulimatolo@gmail.com>
2022-11-01 13:32:28 +0000

    Updates product manager class

--- assignment_3/product_abstract.py
+++ assignment_3/product_abstract.py
@@ -35,29 +35,29 @@ class ProductAbstract(ABC):
 
 class ProductManager(ProductAbstract):
     def create_product(self, product: Product):
-        print("product created")
+        print(f"new product: {product} created now")
 
     def edit_product(self, name):
-        print("product edited")
+        print(f"{name} product edited")
 
     def get_product_by_id(self, id):
-        print("product id")
+        print(f"product id is: {id}")
 
     def get_all_products(self):
         print("names of all print")
 
     def upload_product_image(self, image):
-        print("image uploaded")
+        print(f"image uploaded to{image}")
 
     def delete_product(self, id):
-        print("product deleted")
+        print(f"Product id: {id} has been deleted successfully")
 
 
 product1 = ProductManager()
 
 
-product1.edit_product()
-product1.get_product_by_id()
+product1.edit_product("Chocolate")
+product1.get_product_by_id(123456)
 product1.get_all_products()
-product1.upload_product_image()
-product1.delete_product()
+product1.upload_product_image("path/xyz")
+product1.delete_product(123)
