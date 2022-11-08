00b5ed28073495b3e3ae965a9f89cc6f12fd735b
gmatolo <mulimatolo@gmail.com>
2022-11-08 10:49:33 +0000

    create a contactBook Abstract class

--- contact_book.py
+++ contact_book.py
@@ -1,9 +1,16 @@
 from abc import ABC, abstractmethod
 
-#create a contact details class
+
+# create a contact details class
 class ContactDetails:
     first_name = ""
     second_name = ""
     email_address = ""
     phone_number = ""
 
+
+# create a contactBookAddress to work as a blueprint
+class ContactBookAbstract(ABC):
+    @abstractmethod
+    def create_contact(self, contact: ContactDetails):
+        pass
