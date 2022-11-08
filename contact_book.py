de14307bedb85b4aeef2bdd3a2ecd46bd529a520
gmatolo <mulimatolo@gmail.com>
2022-11-08 10:36:27 +0000

    Adds contact details class

--- contact_book.py
+++ contact_book.py
@@ -1,1 +1,9 @@
 from abc import ABC, abstractmethod
+
+#create a contact details class
+class ContactDetails:
+    first_name = ""
+    second_name = ""
+    email_address = ""
+    phone_number = ""
+
