1c962e9e138d9c80c488bc95e115fd9c12bb6523
gmatolo <mulimatolo@gmail.com>
2022-11-08 10:56:44 +0000

    Add contact manager class

--- contact_book.py
+++ contact_book.py
@@ -14,3 +14,21 @@ class ContactBookAbstract(ABC):
     @abstractmethod
     def create_contact(self, contact: ContactDetails):
         pass
+
+
+class ContactManager(ContactDetails, ContactBookAbstract):
+    def create_contact(self):
+        self.first_name = "Gerald"
+        self.second_name = "Gerald"
+        self.phone_number = "+254719749633"
+        self.email_address = "gerald.matolo@meltwater.org"
+        contact_details = {
+            "Name: ": f"{self.first_name} {self.second_name}",
+            "Email: ": self.email_address,
+            "Phone Number: ": self.phone_number,
+        }
+        # print(f"Your contact details are as: {contact_details.items()}")
+        for k, v in contact_details.items():
+            print(k, v)
+
+
